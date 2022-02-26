5a1085310243bb102abfeddc97cbc2543e532356
Omar <76620155+RepubblicaTech@users.noreply.github.com>
2022-02-26 15:31:26 +0100

    A Little bugfix

--- OS.py
+++ OS.py
@@ -8,33 +8,31 @@ user_name = input()
 if user_name == settings.user:
     print("Enter password:")
     password = input()
-    if password != settings.passwd:
+    if password == settings.passwd:
+        print("Welcome to the Developer Beta 4 of PyOS 0.1!")
+        print("Type <help> to see available commands.")
+        while 2 > 1:
+            cwd = input(settings.user + "@pyos # ")
+
+        if cwd == "update":
+            os.system('pip install --upgrade pip')
+            os.system('pip install --upgrade tqdm')
+            os.system('pip install --upgrade pathlib')
+            os.system('pip install --upgrade requests')
+
+        if cwd == "print":
+            print("Welcome to the Print Shell!")
+            input(settings.user + "> ")
+
+        if cwd == "help":
+            print("3 available commands:")
+            print("")
+            print("help         See available commands")
+            print("print        Works like <echo> in Windows, opens the Print Shell")
+            print("update       Checks for updates, still doesn't update OS")
+    else:
         print("Incorrect password. Close this windows and reopen OS.py")
         input()
 else:
     print("Incorrect username. Quit and reopen OS.py")
     input("Press Enter key to quit...")
-
-print("Welcome to the Developer Beta 4 of PyOS 0.1!")
-print("Type <help> to see available commands.")
-while 2 > 1:
-    cwd = input(settings.user + "@pyos # ")
-
-if cwd == "update":
-    os.system('pip install --upgrade pip')
-    os.system('pip install --upgrade tqdm')
-    os.system('pip install --upgrade pathlib')
-    os.system('pip install --upgrade requests')
-
-if cwd == "print":
-    print("Welcome to the Print Shell!")
-    input(settings.user + "> ")
-
-if cwd == "help":
-    print("3 available commands:")
-    print("")
-    print("help         See available commands")
-    print("print        Works like <echo> in Windows, opens the Print Shell")
-    print("update       Checks for updates, still doesn't update OS")
-
-
