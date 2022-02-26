678fc71d04be441fbe53e9d91ce957a47022b981
Omar <76620155+RepubblicaTech@users.noreply.github.com>
2022-02-26 15:30:11 +0100

    A Little bugfix

--- OS.py
+++ OS.py
@@ -8,17 +8,17 @@ user_name = input()
 if user_name == settings.user:
     print("Enter password:")
     password = input()
-    if password == settings.passwd:
-        print("Welcome to the Developer Beta 4 of PyOS 0.1!")
-        print("Type <help> to see available commands.")
-        while 2 > 1:
-            cwd = input(settings.user + "@pyos # ")
-    else:
+    if password != settings.passwd:
         print("Incorrect password. Close this windows and reopen OS.py")
         input()
 else:
     print("Incorrect username. Quit and reopen OS.py")
     input("Press Enter key to quit...")
+
+print("Welcome to the Developer Beta 4 of PyOS 0.1!")
+print("Type <help> to see available commands.")
+while 2 > 1:
+    cwd = input(settings.user + "@pyos # ")
 
 if cwd == "update":
     os.system('pip install --upgrade pip')
