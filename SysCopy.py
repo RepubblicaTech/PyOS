56b60dc33ea3b96cb79f1f637c0f4fd16dfd4080
Omar <76620155+RepubblicaTech@users.noreply.github.com>
2022-02-26 14:00:23 +0100

    Updating

--- SysCopy.py
+++ SysCopy.py
@@ -8,9 +8,6 @@ for i in tqdm(range(100), desc='Copying system files'):
 for i in tqdm(range(100), desc='Installing required packages'):
     sleep(0.05)
 
-for i in tqdm(range(100), desc='Installing patches'):
-    sleep(0.003)
-
 for i in tqdm(range(100), desc='Setting up environment'):
     sleep(0.03)
 
