fcc8a1ba145ddfb3738952ebeef584b5e9ea7b3f
Omar <76620155+RepubblicaTech@users.noreply.github.com>
2022-02-26 17:16:37 +0100

    Update PrShell.py

--- PrShell.py
+++ PrShell.py
@@ -1,4 +1,5 @@
 import settings
+import os
 
 print("Welcome to the Print Shell!")
 print("Type EVERYTHING YOU WANT to print on the screen.")
@@ -6,6 +7,6 @@ print("Type <exit> to quit.")
 while 3 > 2:
     pr = input(settings.user + "> ")
     if pr == "exit":
-        os.system
+        os.system('cls')
     else:
         print(pr)
