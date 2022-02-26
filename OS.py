d6e6a64f0ef1749cdb181acab179999b6b596f74
Omar <76620155+RepubblicaTech@users.noreply.github.com>
2022-02-26 13:33:57 +0100

    Update OS.py

--- OS.py
+++ OS.py
@@ -1,18 +1,19 @@
 import settings
-from settings import user
 import os
 
 print("Welcome to PythonOS!")
 print("PythonOS login:")
 user_name = input()
 
-if user_name == user:
+if user_name == settings.user:
     print("Enter password:")
     password = input()
     if password == settings.passwd:
         print("Welcome to the Developer Beta 4 of PyOS 0.1!")
         print("Type <help> to see available commands.")
-        cwd = input(settings.user + "@pyos # ")
+
+        def cwd():
+            input(settings.user + "@pyos # ")
     else:
         print("Incorrect password. Close this windows and reopen OS.py")
         input()
