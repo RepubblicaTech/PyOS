3fc99c916dc4fa8ef288a7acfb91654ddf06311e
Setr0 <filippolizzipiggy@gmail.com>
2022-11-04 18:50:31 +0100

    Added the Linux compatibility

--- Install.py
+++ Install.py
@@ -1,37 +1,3 @@
-import time, os, json
-from System import version
-
-obj = {}
-
-print("Please wait, " + version.name_full + "Installer is installing required packages...")
-time.sleep(0.5)
-print("")
-print("")
-
-os.system('python -m pip install --upgrade pip')
-os.system('pip install tqdm')
-time.sleep(0.3)
-os.system('cls')
-
-print("Welcome to " + version.name + version.about + " Installer!")
-print("You will configure the system for your needs.")
-print("")
-print("Enter username:")
-
-user = input()
-
-print("")
-print("Enter password (leave blank for none):")
-
-passwd = input()
-
-obj["PyOS_Env"] = [{"Username" : user, "Password" : passwd}]
-pyos_json = open("System/user.json", "w")
-
-json.dump(obj, pyos_json)
-
-print("")
-os.system('cd System && SysCopy.py')
 import time, os, json, platform
 from System import version
 
