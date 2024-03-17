63d0b3c0871a50cfabb620811286d1e6e9e0c95c
RepubblicaTech <elshorbagy.omar@gozzi-olivetti.org>
2024-03-17 19:33:12 +0100

    Install tqdm module if not present

--- System/install.py
+++ System/install.py
@@ -1,10 +1,11 @@
-import time, json
+import time, json, System.OS.libs.pip as pip
 
 try:
     from tqdm import tqdm
 except ImportError:
-    print("Package tqdm is not installed. Quitting...")
+    print("Package tqdm is not installed. Installing...")
     time.sleep(1)
+    pip.install('tqdm')
     exit(1)
 
 
@@ -13,31 +14,19 @@ class Setup:
         self.obj = {}
         self.username = ""
         self.password = ""
-        Setup.warning(self)
+        print("WARNING: THIS IS A STRIPPED-DOWN VERSION OF PYTHONOS,\nUSE THIS FOR TESTING PURPOSES ONLY.")
         print("Welcome to PythonOS version Next installer!")
         print("Please configure at least one user to make the environment work")
-        Setup.configuration(self)
-        Setup.dumpConfig(self)
-        print("Install finished! You can quit the installer by pressing Enter then start launch.py")
-        input()
-        exit(0)
-        
-    def warning(self):
-        print("WARNING: THIS IS A STRIPPED-DOWN VERSION OF PYTHONOS,\nUSE THIS FOR TESTING PURPOSES ONLY.")
-
-    def configuration(self):
         self.username = input("Enter your username: ")
         self.password = input("Enter a password (optional): ")
         input("If you are sure to continue, press Enter.")
-        Setup.start(self)
-
-    def start(self):
         for i in tqdm(range(100), desc='Setting up PythonOS environment'):
             time.sleep(0.03)
-
-    def dumpConfig(self):
         self.obj["PyOS_Env"] = [{"Username": self.username, "Password": self.password}]
         self.pyos_json = open("System/users.json", "w")
         json.dump(self.obj, self.pyos_json, indent=2)
+        print("Install finished! You can quit the installer by pressing Enter then start launch.py")
+        input()
+        exit(0)
 
         
