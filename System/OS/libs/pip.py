5ab736fda682cd50b0d5ca2afc3a63b62debf501
RepubblicaTech <elshorbagy.omar@gozzi-olivetti.org>
2024-03-23 11:24:52 +0100

    Update 'pip' library

--- System/OS/libs/pip.py
+++ System/OS/libs/pip.py
@@ -2,24 +2,26 @@ import os, sys, subprocess
 
 system = sys.platform
 
-def install(package):
-    if system == "win32" or system == "darwin":
-        os.system('python -m pip install ' + package)
-    elif system == "linux":
-        try:
-            subprocess.run(['pip'], check = True)
-        except subprocess.CalledProcessError:
-            os.system('sudo apt install -y python3-pip')
-        
-        os.system('pip3 install ' + package)
+def install(*packages):
+    for package in packages:
+        if system == "win32" or system == "darwin":
+            os.system('python -m pip install ' + packages)
+        elif system == "linux":
+            try:
+                subprocess.run(['pip'], check = True)
+            except subprocess.CalledProcessError:
+                os.system('sudo apt install -y python3-pip')
 
-def upgrade(package):
-    if system == "win32" or system == "darwin":
-        os.system('python -m pip install --upgrade ' + package)
-    elif system == "linux":
-        try:
-            subprocess.run(['pip'], check = True)
-        except subprocess.CalledProcessError:
-            os.system('sudo apt install python3-pip -y')
-        
-        os.system('pip3 install --upgrade ' + package)
+            os.system('pip3 install ' + packages)
+
+def upgrade(*packages):
+    for package in packages:
+        if system == "win32" or system == "darwin":
+            os.system('python -m pip install --upgrade ' + packages)
+        elif system == "linux":
+            try:
+                subprocess.run(['pip'], check = True)
+            except subprocess.CalledProcessError:
+                os.system('sudo apt install python3-pip -y')
+            
+            os.system('pip3 install --upgrade ' + packages)
